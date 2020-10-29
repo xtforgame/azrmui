4017071db28bde38337bc6c2b8879a0063c0b974
RickChen <xtforgame@gmail.com>
2020-10-29 22:48:14 +0800

    refine EnhancedTable

--- src/library/core/Tables/EnhancedTable.js
+++ src/library/core/Tables/EnhancedTable.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-array-index-key */
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -12,10 +12,10 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import EnhancedTableHead from './EnhancedTableHead';
-import MoreActionMenuButton from '~/core/Buttons/MoreActionMenuButton';
-import ProgressWithMask from '~/core/Progress/ProgressWithMask';
+import MoreActionMenuButton from '../Buttons/MoreActionMenuButton';
+import ProgressWithMask from '../Progress/ProgressWithMask';
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
     width: '100%',
     overflowX: 'auto',
@@ -42,241 +42,202 @@ const styles = theme => ({
   detailCell: {
     backgroundColor: theme.palette.background.default,
   },
-});
+}));
 
-class EnhancedTable extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  static updateState({
-    columns,
-    columnSizes,
-    rows = [],
-    order,
-    orderBy,
-  }) {
+export default (props) => {
+  const {
+    sortTip = 'Sort',
+    withDetail,
+    getActionMenuItems,
+    loading,
+    loadingRows,
+    renderEmptyRows,
+
+    renderRowDetail,
+
+    defaultSortBy,
+
+    rows: rowsProp,
+    order: orderProp,
+    orderBy: orderByProp,
+    columns: columnsProp,
+    columnSizes: columnSizesProp,
+
+    page: pageProp = 0,
+    rowsPerPage: rowsPerPageProp = 10,
+    rowsPerPageOptions: rowsPerPageOptionsProp = [10, 25, 50, 100],
+  } = props;
+
+  const classes = useStyles();
+
+  const [rows, setRows] = useState(rowsProp);
+  const [order, setOrder] = useState(orderProp);
+  const [orderBy, setOrderBy] = useState(orderByProp);
+  const [columns, setColumns] = useState(columnsProp);
+  const [columnSizes, setColumnSizes] = useState(columnSizesProp);
+
+  const [page, setPage] = useState(pageProp);
+  const [rowsPerPage, setRowsPerPage] = useState(rowsPerPageProp);
+  const [expandedMap, setExpandedMap] = useState({});
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+  const handleRequestSort = (_, property) => {
+    let newOrderBy = property || defaultSortBy;
+    let newOrder = 'desc';
+
+    if (orderBy === property) {
+      if (order === 'desc') {
+        newOrder = 'asc';
+      } else if (orderBy !== defaultSortBy) {
+        newOrderBy = defaultSortBy;
+        newOrder = 'asc';
+      } else {
+        newOrder = 'desc';
+      }
+    }
+
     let sortedRows = [...rows]; // eslint-disable-line no-unused-vars
-
     const columnMap = {};
     columns.forEach((column) => {
       columnMap[column.id] = column;
     });
 
     let compare = (a, b, orderBy) => a[orderBy] < b[orderBy];
-    if (columnMap[orderBy] && columnMap[orderBy].numeric) {
+    if (columnMap[newOrderBy] && columnMap[newOrderBy].numeric) {
       compare = (a, b, orderBy) => parseFloat(a[orderBy]) < parseFloat(b[orderBy]);
     }
-    if (order && orderBy) {
-      sortedRows = order === 'desc'
-        ? rows.sort((a, b) => (compare(b, a, orderBy) ? -1 : 1))
-        : rows.sort((a, b) => (compare(a, b, orderBy) ? -1 : 1));
+    if (newOrder && newOrderBy) {
+      sortedRows = newOrder === 'desc'
+        ? rows.sort((a, b) => (compare(b, a, newOrderBy) ? -1 : 1))
+        : rows.sort((a, b) => (compare(a, b, newOrderBy) ? -1 : 1));
     }
 
-    return {
-      columns,
-      columnSizes,
-      columnMap,
-      rows: sortedRows,
-      order,
-      orderBy,
-    };
-  }
+    setOrder(newOrder);
+    setOrderBy(newOrderBy);
+    setRows(sortedRows);
 
-  constructor(...args) {
-    super(...args);
-
-    this.state = {
-      page: 0,
-      rowsPerPage: 5,
-      expanded: {},
-      ...EnhancedTable.updateState({
-        columns: this.props.columns,
-        columnSizes: this.props.columnSizes,
-        rows: this.props.rows,
-        order: 'asc',
-        orderBy: this.props.defaultSortBy,
-      }),
-    };
-  }
-
-  static getDerivedStateFromProps(props, prevState) {
-    if ((props.rows && (props.rows !== prevState.rows))
-      || (props.order && (props.order !== prevState.order))
-      || (props.orderBy && (props.orderBy !== prevState.orderBy))
-      || (props.columns && (props.columns !== prevState.columns))
-      || (props.columnSizes && (props.columnSizes !== prevState.columnSizes))
-    ) {
-      const rows = props.rows || prevState.rows;
-      const order = props.order || prevState.order;
-      const orderBy = props.orderBy || prevState.orderBy;
-      const columns = props.columns || prevState.columns;
-
-      return EnhancedTable.updateState({
-        columns, rows, order, orderBy,
-      });
-    }
-
-    // No state update necessary
-    return null;
-  }
-
-  handleChangePage = (event, page) => {
-    this.setState({ page });
+    setColumns(columns);
+    setColumnSizes(columnSizes);
   };
 
-  handleChangeRowsPerPage = (event) => {
-    this.setState({ rowsPerPage: event.target.value });
-  };
+  useEffect(() => {
+    handleRequestSort();
+  }, [
+    rowsProp,
+    orderProp,
+    orderByProp,
+    columnsProp,
+    columnSizesProp,
+  ]);
 
-  handleRequestSort = (_, property) => {
-    let orderBy = property || this.props.defaultSortBy;
-    let order = 'desc';
-
-    if (this.state.orderBy === property) {
-      if (this.state.order === 'desc') {
-        order = 'asc';
-      } else if (orderBy !== this.props.defaultSortBy) {
-        orderBy = this.props.defaultSortBy;
-        order = 'asc';
-      } else {
-        order = 'desc';
-      }
-    }
-
-    this.setState(EnhancedTable.updateState({
-      columns: this.props.columns,
-      columnSizes: this.props.columnSizes,
-      rows: this.state.rows,
-      order,
-      orderBy,
-    }));
-  };
-
-  toggleDetail(row) {
-    if (this.state.expanded[row.id]) {
-      return this.setState({
-        expanded: {},
-      });
-    }
-    return this.setState({
-      expanded: {
+  const toggleDetail = (row) => {
+    if (expandedMap[row.id]) {
+      setExpandedMap({});
+    } else {
+      setExpandedMap({
         [row.id]: true,
-      },
-    });
-  }
+      });
+    }
+  };
 
-  render() {
-    const {
-      classes,
-      withDetail,
-      getActionMenuItems,
-      columns,
-      columnSizes,
-      loading,
-      loadingRows,
-      renderEmptyRows,
-    } = this.props;
-    const {
-      order, orderBy, rowsPerPage, page,
-    } = this.state;
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, this.state.rows.length - page * rowsPerPage);
-
-    return (
-      <Paper className={classes.root}>
-        <Table className={columnSizes ? classes.tableFixedLayout : classes.table}>
-          <EnhancedTableHead
-            withDetail={withDetail}
-            withActions={!!getActionMenuItems}
-            columns={columns}
-            columnSizes={columnSizes}
-            sortTip="Sort"
-            order={order}
-            orderBy={orderBy}
-            onRequestSort={this.handleRequestSort}
-          />
-          <TableBody>
-            {this.state.rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-              const expanded = this.state.expanded[row.id];
-              const options = {
-                columns,
-              };
-              return (
-                <React.Fragment key={row.id}>
-                  <TableRow>
-                    { withDetail
-                      && (
-                        <TableCell padding="checkbox" className={classes.iconCell}>
-                          <IconButton
-                            onClick={() => { this.toggleDetail(row); }}
-                          >
-                            {expanded ? <ExpandMoreIcon /> : <ChevronRightIcon />}
-                          </IconButton>
-                        </TableCell>
-                      )
-                    }
-                    {columns.map((column) => {
-                      const renderFunction = row.renderCell
-                        || column.renderRowCell
-                        || ((columnId, row) => row[columnId]);
-                      return (
-                        <TableCell
-                          key={column.id}
-                          align={column.numeric ? 'right' : 'left'}
-                          padding={column.padding || 'default'}
-                          className={column.cellClassName}
-                        >
-                          {renderFunction(column.id, row, options)}
-                        </TableCell>
-                      );
-                    })}
-                    { getActionMenuItems
-                      && (
-                        <TableCell padding="checkbox" className={classes.actionsCell}>
-                          <MoreActionMenuButton
-                            getActionMenuItems={getActionMenuItems}
-                          />
-                        </TableCell>
-                      )
-                    }
-                  </TableRow>
-                  {withDetail && expanded && (
-                    <TableRow>
-                      <TableCell
-                        colSpan={columns.length + (+withDetail) + (+!!getActionMenuItems)}
-                        className={classes.detailCell}
-                      >
-                        {this.props.renderRowDetail && this.props.renderRowDetail(row, options)}
-                      </TableCell>
-                    </TableRow>
-                  )}
-                </React.Fragment>
-              );
-            })}
-            {renderEmptyRows && emptyRows > 0
-            && Array.apply(null, { length: emptyRows }) // eslint-disable-line prefer-spread
-            .map((_, i) => (
-              <TableRow key={`empty-${i}`}>
-                <TableCell colSpan={columns.length + (+withDetail) + (+!!getActionMenuItems)} />
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        {!loading && loadingRows && <ProgressWithMask delay={100} />}
-        <TablePagination
-          component="div"
-          count={this.state.rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          backIconButtonProps={{
-            'aria-label': 'Previous Page',
-          }}
-          nextIconButtonProps={{
-            'aria-label': 'Next Page',
-          }}
-          onChangePage={this.handleChangePage}
-          onChangeRowsPerPage={this.handleChangeRowsPerPage}
+  return (
+    <Paper className={classes.root}>
+      <Table className={columnSizes ? classes.tableFixedLayout : classes.table}>
+        <EnhancedTableHead
+          withDetail={withDetail}
+          withActions={!!getActionMenuItems}
+          columns={columns}
+          columnSizes={columnSizes}
+          sortTip={sortTip}
+          order={order}
+          orderBy={orderBy}
+          onRequestSort={handleRequestSort}
         />
-        {loading && <ProgressWithMask delay={100} />}
-      </Paper>
-    );
-  }
-}
-
-export default withStyles(styles)(EnhancedTable);
+        <TableBody>
+          {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+            const expanded = expandedMap[row.id];
+            const options = {
+              columns,
+            };
+            return (
+              <React.Fragment key={row.id}>
+                <TableRow>
+                  { withDetail
+                    && (
+                      <TableCell padding="checkbox" className={classes.iconCell}>
+                        <IconButton
+                          onClick={() => { toggleDetail(row); }}
+                        >
+                          {expanded ? <ExpandMoreIcon /> : <ChevronRightIcon />}
+                        </IconButton>
+                      </TableCell>
+                    )
+                  }
+                  {columns.map((column) => {
+                    const renderFunction = row.renderCell
+                      || column.renderRowCell
+                      || ((columnId, row) => row[columnId]);
+                    return (
+                      <TableCell
+                        key={column.id}
+                        align={column.numeric ? 'right' : 'left'}
+                        padding={column.padding || 'default'}
+                        className={column.cellClassName}
+                      >
+                        {renderFunction(column.id, row, options)}
+                      </TableCell>
+                    );
+                  })}
+                  { getActionMenuItems
+                    && (
+                      <TableCell padding="checkbox" className={classes.actionsCell}>
+                        <MoreActionMenuButton
+                          getActionMenuItems={getActionMenuItems}
+                        />
+                      </TableCell>
+                    )
+                  }
+                </TableRow>
+                {withDetail && expanded && (
+                  <TableRow>
+                    <TableCell
+                      colSpan={columns.length + (+withDetail) + (+!!getActionMenuItems)}
+                      className={classes.detailCell}
+                    >
+                      {renderRowDetail && renderRowDetail(row, options)}
+                    </TableCell>
+                  </TableRow>
+                )}
+              </React.Fragment>
+            );
+          })}
+          {renderEmptyRows && emptyRows > 0
+          && Array.apply(null, { length: emptyRows }) // eslint-disable-line prefer-spread
+          .map((_, i) => (
+            <TableRow key={`empty-${i}`}>
+              <TableCell colSpan={columns.length + (+withDetail) + (+!!getActionMenuItems)} />
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      {!loading && loadingRows && <ProgressWithMask delay={100} />}
+      <TablePagination
+        component="div"
+        count={rows.length}
+        page={page}
+        rowsPerPage={rowsPerPage}
+        rowsPerPageOptions={rowsPerPageOptionsProp}
+        backIconButtonProps={{
+          'aria-label': 'Previous Page',
+        }}
+        nextIconButtonProps={{
+          'aria-label': 'Next Page',
+        }}
+        onChangePage={(_, p) => setPage(p)}
+        onChangeRowsPerPage={e => setRowsPerPage(e.target.value)}
+      />
+      {loading && <ProgressWithMask delay={100} />}
+    </Paper>
+  );
+};
